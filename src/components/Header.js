448567cb711f45fda6402e57b80fcca64dd05dbc
Darius David <44746797+dariusdv@users.noreply.github.com>
2021-03-27 19:02:01 +0200

    AboutMe completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ export const Header = () => {
 	return (
 		<div className="header-wraper">
 			<div className="main-info">
-				<h1>web development and websites promotions</h1>
+				<h1>blockchain and web development</h1>
 				<Typed
 					className="typed-text"
-					strings={[ 'React', 'Blockchain', 'Web Development' ]}
+					strings={[ 'React', 'Blockchain', 'Web Development', 'JavaScript' ]}
 					typeSpeed={40}
 					backSpeed={60}
 					loop
